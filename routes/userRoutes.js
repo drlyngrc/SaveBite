8d0b4102e1bfb0ed76b0b27c0c5b5223245913c3
baddddddddd <eluxeofficial@gmail.com>
2025-03-23 17:54:13 +0800

    Reflect actual profile information in /profile page

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
+import { displayProfile } from "../controllers/userController.js";
+import UserService from "../services/UserService.js";
 const router = express.Router();
 
-router.get("/profile", (req, res) => {
-    res.render("main/profile.ejs", { currentRoute: '/profile' });
+const userService = new UserService();
+
+router.get("/profile",  userService.checkAuth.bind(userService), async (req, res) => {
+    return await displayProfile(req, res);
 });
 
 export default router;
