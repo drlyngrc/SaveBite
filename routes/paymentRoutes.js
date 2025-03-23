c25b973bc2839aefc5360adcd7789f85c4f9da19
baddddddddd <eluxeofficial@gmail.com>
2025-03-23 16:39:02 +0800

    Add sales and purchase history

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -5,7 +5,14 @@ import UserService from "../services/UserService.js";
 const router = express.Router();
 const userService = new UserService();
 
-// router.get("/order/history", userService.checkAuth.bind(userService), getAllOrders);
+router.get("/history/sales", userService.checkAuth.bind(userService), (req, res) => {
+  res.render("history/sales.ejs", { currentRoute: "/history/sales" });
+});
+
+router.get("/history/purchase", userService.checkAuth.bind(userService), (req, res) => {
+  res.render("history/purchase.ejs", { currentRoute: "/history/purchase" });
+});
+
 router.post(
   "/api/payment/process-payment",
   userService.checkAuth.bind(userService),
